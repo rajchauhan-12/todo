656e79b37dc37badce0598d7e531bdc19f0a31d4
rajchuahn-12 <rajchuahan25112004@gmail.com>
2026-06-03 19:07:09 +0530

    initial commit

--- starter/client/src/index.js
+++ starter/client/src/index.js
@@ -1,21 +1,32 @@
 import { Auth0Provider } from '@auth0/auth0-react'
 import React from 'react'
-import ReactDOM from 'react-dom'
-import 'semantic-ui-css/semantic.min.css'
+import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
-ReactDOM.render(
-  <Auth0Provider
-    domain={'dev-yiid3pn1blque42e.us.auth0.com'}
-    clientId={'VOyLkD5kcPo0DOrmYVd3hgOF6Ztu7rXL'}
-    redirectUri={window.location.origin}
-    audience={`https://sp76174uyh.execute-api.us-east-1.amazonaws.com/dev`}
-    scope="read:todo write:todo delete:todo"
-  >
-    <App />
-  </Auth0Provider>,
-  document.getElementById('root')
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+<Auth0Provider
+  domain={domain}
+  clientId={clientId}
+  redirectUri={window.location.origin}
+  audience="https://your-todo-api"
+  scope="openid profile email read:todo write:todo delete:todo"
+  cacheLocation="localstorage"
+  useRefreshTokens={true}
+  onRedirectCallback={(appState) => {
+    window.history.replaceState(
+      {},
+      document.title,
+      appState?.returnTo || window.location.pathname
+    )
+  }}
+>
+  <App />
+</Auth0Provider>
 )
